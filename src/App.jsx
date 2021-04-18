8f0f6caacf81f7a53c99191d2afbcff985179efd
L3p1989 <phogan110@live.com>
2021-04-18 13:55:40 -0700

    imported Timeline and added to div

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ChirpInput from './components/ChirpInput';
+import Timeline from "./components/Timeline";
 
 const App = () => {
     return(
         <div>
-            <h1 className="text-center">Chirper</h1>
+            <h1 className="text-center mb-5">Chirper</h1>
             <ChirpInput />
+            <Timeline />
         </div>
     );
 };
