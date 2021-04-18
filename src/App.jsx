ba6a0729150cd430ab4d37fd1ed3f18234130dc1
L3p1989 <phogan110@live.com>
2021-04-18 16:45:37 -0700

    set up state for chirps, passed setChirps as prop into chirpInput, and chirps into Timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChirpInput from './components/ChirpInput';
 import Timeline from "./components/Timeline";
 
 const App = () => {
+
+    const [chirps, setChirps] = useState([]);
+
     return(
         <div>
             <h1 className="text-center mb-5">Chirper</h1>
-            <ChirpInput />
-            <Timeline />
+            <ChirpInput setChirps={setChirps} />
+            <Timeline chirps={chirps} />
         </div>
     );
 };
