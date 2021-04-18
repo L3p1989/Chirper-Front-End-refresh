b4ab33400f930cbb419f1712b0045990d6b0af0b
L3p1989 <phogan110@live.com>
2021-04-18 16:49:38 -0700

    set up logic to push chirps prop into state chirpArray upon change

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Chirp from './Chirp';
 
-const Timeline = props => {
+const Timeline = ({ chirps }) => {
+
+    const [chirpArray, setChirpArray] = useState([]);
+
+    useEffect(() => {
+        setChirpArray(chirps)
+    }, [chirps]);
+
     return(
         <div className="container" id="timeline">
-           <Chirp text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." name="Someone famous in Source Title" />
-           <Chirp text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." name="Someone famous in Source Title" />
-           <Chirp text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." name="Someone famous in Source Title" />
+            {chirpArray.map(val => (
+                <Chirp key={val.id} name={val.name} text={val.text} />
+            ))}
+            <Chirp key="2" name="Someone famous in Source Title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." />
+            <Chirp key="1" name="Someone famous in Source Title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." />
+            <Chirp key="0" name="Someone famous in Source Title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." />
         </div>
     );
 }
