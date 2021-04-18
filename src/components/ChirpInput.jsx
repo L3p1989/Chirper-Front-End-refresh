fdc117ee3f24400a9ed7d4b82380c4a1b25928e3
L3p1989 <phogan110@live.com>
2021-04-18 16:46:58 -0700

    set up logic to pass array of objects back to APP

--- src/components/ChirpInput.jsx
+++ src/components/ChirpInput.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ChirpInput = () => {
+const ChirpInput = ({ setChirps }) => {
+
+    const [id, setId] = useState(3);
+
+    const [name, setName] = useState('');
+
+    const [text, setText] = useState('');
+
+    const [chirpsArray, setChirpsArray] = useState([]);
+
+    const handleClick = (e) => {
+        let newChirp = {
+            id: id,
+            name: name,
+            text: text
+        };
+
+        let array = chirpsArray;
+
+        array.unshift(newChirp);
+
+        setChirpsArray(array);
+
+        setId(id + 1);
+
+        setChirps(chirpsArray);
+        
+        e.preventDefault()
+    }
 
     return(
         <div className="container">
             <form>
                 <div className="form-group">
-                    <input type="text" id="chirp-name" placeholder="Username" />
+                    <input type="text" id="chirp-name" placeholder="Username" onChange={e => setName(e.target.value)} />
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" id="chirp-text" placeholder="Say something"/>
+                    <input type="text" className="form-control" id="chirp-text" placeholder="Say something" onChange={e => setText(e.target.value)}/>
                 </div>
                 <div className="form-group">
-                    <button type="submit" className="btn btn-primary" id="chirp-btn">Chirp It!</button>
+                    <button type="submit" className="btn btn-primary" id="chirp-btn" onClick={handleClick}>Chirp It!</button>
                 </div>
             </form>
         </div>
