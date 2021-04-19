fa2b3e19f3b6a0ab6eb9eec418297e4814e55ac0
L3p1989 <phogan110@live.com>
2021-04-19 16:54:18 -0700

    removed state, removed useless imports, and removed hard coded components. set map to use prop

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,22 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Chirp from './Chirp';
 
 const Timeline = ({ chirps }) => {
 
-    const [chirpArray, setChirpArray] = useState([]);
-
-    useEffect(() => {
-        setChirpArray(chirps)
-    }, [chirps]);
-
     return(
         <div className="container" id="timeline">
-            {chirpArray.map(val => (
-                <Chirp key={val.id} name={val.name} text={val.text} />
+            {chirps.map(chirp => (
+                <Chirp key={chirp.id} name={chirp.name} text={chirp.text} />
             ))}
-            <Chirp key="2" name="Someone famous in Source Title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." />
-            <Chirp key="1" name="Someone famous in Source Title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." />
-            <Chirp key="0" name="Someone famous in Source Title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." />
         </div>
     );
 }
