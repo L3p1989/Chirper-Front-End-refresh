52be8e1dc806c3e23872a654d5e4f99c2a1eef2f
L3p1989 <phogan110@live.com>
2021-04-19 16:55:19 -0700

    set up state to have 3 values to start with

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,28 @@ import Timeline from "./components/Timeline";
 
 const App = () => {
 
-    const [chirps, setChirps] = useState([]);
+    const [chirps, setChirps] = useState([
+        {
+            id: 2,
+            name: "Someone famous in Source Title",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+        },
+        {
+            id: 1,
+            name: "Someone famous in Source Title",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+        },
+        {
+            id: 0,
+            name: "Someone famous in Source Title",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+        }
+    ]);
 
     return(
         <div>
             <h1 className="text-center mb-5">Chirper</h1>
-            <ChirpInput setChirps={setChirps} />
+            <ChirpInput chirps={chirps} setChirps={setChirps} />
             <Timeline chirps={chirps} />
         </div>
     );
